1bd625c28352ebafbe04723897c3654392769086
Carlos Quinones <CQuinones@Qzion.net>
2017-08-23 15:45:35 -0400

    Ep. 2 - Test Driving Threads

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -13,7 +13,8 @@ class ThreadsTest extends TestCase
 
     public function a_user_can_browse_threads ()
     {
-        $response = $this->get('/threads');
+        $response = $this->get('/');
+
         $response->assertStatus(200);
     }
 }
