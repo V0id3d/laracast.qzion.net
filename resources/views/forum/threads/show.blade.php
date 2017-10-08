883ae8423788b4f2d2854f075de236587c3fac78
Carlos Quinones <CQuinones@Qzion.net>
2017-10-08 16:05:56 -0400

    Ep. 4 - A User May Respond to Threads

--- resources/views/forum/threads/show.blade.php
+++ resources/views/forum/threads/show.blade.php
@@ -25,7 +25,11 @@
         @if (auth()->check())
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
-                    test
+                    <form action="POST" action="{{ $thread->path() . '/replies' }}">
+                        <div class="form-group">
+                            <textarea name="body" id="body" class="form-control" placeholder="Have something to say?"></textarea>
+                        </div>
+                    </form>
                 </div>
             </div>
         @endif
